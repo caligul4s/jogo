d8f522944ff1b25e94f770ec4aab8b5927ea7028
caligula <mirytv8@gmail.com>
2025-08-21 09:41:44 -0300

    Update Task.js

--- src/engine/Task.js
+++ src/engine/Task.js
@@ -1,11 +1,11 @@
-// engine/Task.js
 
 export default class Task {
   constructor(type, target) {
     this.type = type;
-    this.target = target; // sprite o game object
+    this.target = target; 
     this.progress = 0;
     this.requiredProgress = 100;
     this.done = false;
   }
 }
+
